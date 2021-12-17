c5e1aef2b8c02abac5d311abca34957fbf0798ff
Rafael Dalcolmo <dalcolmor@student.mville.edu>
2021-12-16 19:24:20 -0500

    Fixed handler error

--- Gengar/Modules/BirthdayCommands.cs
+++ Gengar/Modules/BirthdayCommands.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Gengar.Models;
 using Discord.Commands;
-using Gengar.Handlers;
 using Discord;
 using Discord.Addons.Interactive;
 using Microsoft.EntityFrameworkCore;
