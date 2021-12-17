9c128dca24cf025ff06cd537f69e29153c665ff4
Rafael Dalcolmo <dalcolmor@student.mville.edu>
2021-12-16 19:18:55 -0500

    New date time system

--- Gengar/Services/GengarService.cs
+++ Gengar/Services/GengarService.cs
@@ -1,5 +1,4 @@
-﻿using Gengar.Handlers;
-using Gengar.Models;
+﻿using Gengar.Models;
 using Discord.Commands;
 using Discord.WebSocket;
 using System;
@@ -9,11 +8,14 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Discord;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
 
 namespace Gengar.Services
 {
-	public class GengarService
+	public class GengarService : IHostedService
 	{
+		private static Timer _timer;
 		private readonly DiscordSocketClient _discord;
 		private readonly CommandService _commands;
 		private IServiceProvider _provider;
@@ -24,7 +26,6 @@ namespace Gengar.Services
 			_commands = commands;
 			_provider = provider;
 			_discord.MessageReceived += HandleCommandAsync;
-			DateTimeHandler.DayChanged += DateTimeHandler_DayChanged;
 		}
 
 		public async Task InitializeAsync(IServiceProvider provider)
@@ -40,46 +41,6 @@ namespace Gengar.Services
 				return _dbContext.TblGuilds.ToList();
 			}
 		}
-
-		private async void DateTimeHandler_DayChanged(object sender, DayChangedEventArgs e)
-		{
-			using (var _dbContext = new GengarContext())
-			{
-				foreach (var guild in GetGuildInformation())
-				{
-					var Guild = _discord.GetGuild((ulong)guild.Guildid);
-					var Channel = Guild.GetTextChannel((ulong)guild.Channelid);
-
-					var birthday = _dbContext.TblBirthdays.AsNoTracking().Where(d => d.Birthday.Month == DateTime.Now.Month && d.Birthday.Day == DateTime.Now.Day).ToList();
-
-					foreach (var user in birthday.ToList())
-					{
-						if (Guild.GetUser((ulong)user.Userid) == null)
-						{
-							birthday.Remove(user);
-						}
-					}
-
-					if (birthday.Count > 0)
-					{
-						string _content;
-						if (birthday.Count == 1)
-							_content = "There is 1 birthday today!";
-						else
-							_content = $"There are {birthday.Count} birthdays today!";
-
-
-						await Channel.SendMessageAsync(_content).ConfigureAwait(false);
-
-						foreach (var person in birthday)
-						{
-							await Channel.SendMessageAsync($"It's <@{person.Userid}> birthday today!! Happy birthday!").ConfigureAwait(false);
-						}
-					}
-				}
-			}
-		}
-
 
 		private async Task HandleCommandAsync(SocketMessage arg)
 		{
@@ -109,5 +70,74 @@ namespace Gengar.Services
 				//    await msg.Channel.SendMessageAsync(result.ErrorReason);
 			}
 		}
-	}
+
+        public Task StartAsync(CancellationToken cancellationToken)
+		{
+			TimeSpan interval = TimeSpan.FromHours(24);
+			//calculate time to run the first time & delay to set the timer
+			//DateTime.Today gives time of midnight 00.00
+			var nextRunTime = DateTime.Today.AddDays(1).AddHours(10);
+			var curTime = DateTime.Now;
+			var firstInterval = nextRunTime.Subtract(curTime);
+
+			Action action = () =>
+			{
+				var t1 = Task.Delay(firstInterval);
+				t1.Wait();
+				//Broadcast birthdays
+				BroadcastBirthday(null);
+				//now schedule it to be called every 24 hours for future
+				// timer repeates call to RemoveScheduledAccounts every 24 hours.
+				_timer = new Timer(BroadcastBirthday, null, TimeSpan.Zero,	interval);
+			};
+
+			// no need to await this call here because this task is scheduled to run much much later.
+			Task.Run(action);
+			return Task.CompletedTask;
+		}
+
+		public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+		public void BroadcastBirthday(object state)
+        {
+			using (var _dbContext = new GengarContext())
+			{
+				foreach (var guild in GetGuildInformation())
+				{
+					var Guild = _discord.GetGuild((ulong)guild.Guildid);
+					var Channel = Guild.GetTextChannel((ulong)guild.Channelid);
+
+					var birthday = _dbContext.TblBirthdays.AsNoTracking().Where(d => d.Birthday.Month == DateTime.Now.Month && d.Birthday.Day == DateTime.Now.Day).ToList();
+
+					foreach (var user in birthday.ToList())
+					{
+						if (Guild.GetUser((ulong)user.Userid) == null)
+						{
+							birthday.Remove(user);
+						}
+					}
+
+					if (birthday.Count > 0)
+					{
+						string _content;
+						if (birthday.Count == 1)
+							_content = "There is 1 birthday today!";
+						else
+							_content = $"There are {birthday.Count} birthdays today!";
+
+
+						Channel.SendMessageAsync(_content).Start();
+
+						foreach (var person in birthday)
+						{
+							Channel.SendMessageAsync($"It's <@{person.Userid}> birthday today!! Happy birthday!").Start();
+						}
+					}
+				}
+			}
+		}
+    }
 }
